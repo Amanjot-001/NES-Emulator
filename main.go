73ded5de74d032094ed1b83dbef2350e318598d2
Amanjot-001 <amanjotsingh2309@gmail.com>
2024-11-03 12:23:20 +0530

    reset checks

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"goNESis/cpu"
+)
 
 func main() {
-	fmt.Println("hello nes")
+	cpuInstance := cpu.NewCPU()
+
+	// Power-up state for CPU
+	fmt.Printf("Power-Up State:\n PC: %04X\n S: %02X\n A: %02X\n X: %02X\n Y: %02X\n Status: %08b\n",
+		cpuInstance.PC, cpuInstance.S, cpuInstance.A, cpuInstance.X, cpuInstance.Y, cpuInstance.P)
+
+	// Simulate a reset and print CPU state again
+	cpuInstance.Reset()
+	fmt.Printf("After Reset:\n PC: %04X\n S: %02X\n A: %02X\n X: %02X\n Y: %02X\n Status: %08b\n",
+		cpuInstance.PC, cpuInstance.S, cpuInstance.A, cpuInstance.X, cpuInstance.Y, cpuInstance.P)
 }
