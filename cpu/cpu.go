d40b8800f7be4f1f3299d868261b6096aaef1f31
Amanjot-001 <amanjotsingh2309@gmail.com>
2024-11-03 12:31:45 +0530

    cpu struct fixed

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -1,6 +1,6 @@
 package cpu
 
-// CPU represents the 6502 CPU structure with necessary registers.
+// CPU represents the 6502 structure
 type CPU struct {
 	PC uint16 // Program Counter
 	S  uint8  // Stack Pointer
@@ -9,20 +9,20 @@ type CPU struct {
 	X uint8 // X Register
 	Y uint8 // Y Register
 
-	// Status flags (using single byte for simplicity, each bit can represent a flag)
+	// Status flags (each bit can represent a flag)
 	P uint8
 }
 
 func NewCPU() *CPU {
 	return &CPU{
-		PC: 0xFFFC,
-		S:  0xFD,
-		P:  0b00100100,
+		PC: 0xFFFC,     // PC points to reset vector location
+		S:  0xFD,       // S points to $FD
+		P:  0b00100100, // set I flag to 1 with others as 0 and 3rd bit is always 1
 	}
 }
 
 func (cpu *CPU) Reset() {
-	cpu.PC = 0xFFFC
-	cpu.S -= 3
-	cpu.P |= 0b00000100
+	cpu.PC = 0xFFFC     // re-init to $FFFC
+	cpu.S -= 3          // S decrements by 3 as per behaviour
+	cpu.P |= 0b00000100 // reset sets I flag to 1 (OR to achieve this)
 }
